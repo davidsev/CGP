1ada639558ad54fff08699c159c1f3ac0729e304
David Severwright <david@davidsev.co.uk>
2014-12-18 11:01:42 +0000

    Fixing up un-categorised hosts too.

--- plugin.php
+++ plugin.php
@@ -10,22 +10,27 @@ html_start();
 
 $h = array();
 
+function show_host($host)
+{
+	global $plugin;
+	$plugins = collectd_plugins($host);
+	if(!in_array($plugin, $plugins))
+		continue;
+	printf("<fieldset id=\"%s\">", htmlentities($host));
+	printf("<legend>%s</legend>", htmlentities($host));
+	echo '<div class="graphs">';
+	plugin_header($host, $plugin);
+	graphs_from_plugin($host, $plugin, false);
+	echo '</div>';
+	printf("</fieldset>");
+}
+
 # show all categorized hosts
 if (isset($CONFIG['cat']) && is_array($CONFIG['cat'])) {
 	foreach($CONFIG['cat'] as $cat => $hosts) {
 		$h = array_merge($h, $hosts);
 		foreach($hosts as $host) {
-		
-			$plugins = collectd_plugins($host);
-			if(!in_array($plugin, $plugins))
-				continue;
-			printf("<fieldset id=\"%s\">", htmlentities($host));
-			printf("<legend>%s</legend>", htmlentities($host));
-			echo '<div class="graphs">';
-			plugin_header($host, $plugin);
-			graphs_from_plugin($host, $plugin, false);
-			echo '</div>';
-			printf("</fieldset>");
+			show_host($host);
 		}
 	}
 }
@@ -37,7 +42,9 @@ $uhosts = array_diff($chosts, $h);
 
 # show all uncategorized hosts
 if ($uhosts) {
-	host_summary('uncategorized', $uhosts);
+	foreach($hosts as $host) {
+		show_host($host);
+	}
 }
 
 html_end();
